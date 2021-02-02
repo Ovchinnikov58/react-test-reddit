6901a423b1817d8400a419e7f166bb72ec0ffe8c
Ovchinnikov58 <h1ttch@yandex.ru>
2021-02-02 11:49:46 +0300

    removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Home from './pages/home/Home'
 import Bookmark from './pages/bookmark/Bookmark'
-import Info from './pages/info/Info'
-import Contacts from './pages/contacts/Contacts'
 import NotFound from './pages/notFound/NotFound'
 import Menu from './components/menu/Menu'
 import Layout from "./hoc/Layout/Layout";
@@ -15,8 +13,6 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/bookmark" component={Bookmark}/>
-                <Route path="/info" component={Info}/>
-                <Route path="/contacts" component={Contacts}/>
                 <Route component={NotFound}/>
             </Switch>
         </Layout>
